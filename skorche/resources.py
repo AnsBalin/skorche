7d24e535dc2e36381cce777a08887264a589a156
AnsBalin <ansbalin@gmail.com>
2024-01-28 16:11:15 +0000

    intoduce multiprocessing manager and use it to provide queue instances

---
+++ skorche/resources.py
@@ -0,0 +1,10 @@
+from multiprocessing import Manager
+
+"""
+Global resource for acquiring a multiprocessing queue.
+TODO: migrate this somewhere where it need not be global
+"""
+_mp_manager = Manager()
+
+def get_queue():
+    return _mp_manager.Queue()
