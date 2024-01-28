20f5ac1fc20915999f0d07826164dd2ac9c8dd21
AnsBalin <ansbalin@gmail.com>
2024-01-28 16:12:00 +0000

    use multiprocessing manager to provide queue

--- skorche/queue.py
+++ skorche/queue.py
@@ -1,28 +1,21 @@
 from .node import NodeType, Node
+from .resources import get_queue
 
 class Queue(Node):
     """Wrapper interface for multiprocessing.Manager().Queue()"""
     def __init__(self, name="Queue"):
         super().__init__(NodeType.QUEUE)
         self.name = name
-
-        # TODO: replace temporary list implementation of queue.
-        self.queue = []
-        self.qptr = 0
+        self.queue = get_queue()
 
     def empty(self):
-        return self.qptr >= len(self.queue)  
+        return self.queue.empty()
 
     def put(self, item):
-        self.queue.append(item)
+        self.queue.put(item)
 
     def get(self):
-        if not self.empty():
-            item = self.queue[self.qptr]
-            self.qptr += 1
-            return item
-        else:
-            raise Exception("Cannot get item from empty queue") 
+        return self.queue.get()
 
 
 
